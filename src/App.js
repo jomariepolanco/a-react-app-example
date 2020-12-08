0b8c4b586075939416ba9ece0c03ffdab278619c
jomariepolanco <jomariepolanco@gmail.com>
2020-12-08 14:59:25 -0500

    done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import moment from 'moment';
 import ExampleComponent from './ExampleComponent'
 import TestComponent from './TestComponent'
+//react and moment are both npm packages
+//pulling in specific content from these two packages - like python
+//being imported from node_modules folder
+//example & testcomponent are files from same directory
 
 // Add your code own within the return statement
 
@@ -11,7 +15,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {moment().format('MMMM Do YYYY, hh:mm:ss a')}
+          {/* {moment().format('MMMM Do YYYY, hh:mm:ss a')} */
+          "Now"}
         </header>
         <p className="App-intro">
           In React apps, we write JSX - it looks like HTML, and uses a lot of HTML syntax.
@@ -19,6 +24,7 @@ class App extends Component {
           allows us to add in components, which are separate, self-contained chunks of JSX.
         </p>
         <ExampleComponent />
+        <TestComponent />
 
       </div>
     );
@@ -26,3 +32,5 @@ class App extends Component {
 }
 
 export default App;
+//allowing other files to import things from App.js file
+//App class is the main thing we want to export from this file
